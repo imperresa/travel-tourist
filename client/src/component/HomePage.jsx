8f7d012fcb59735cbe60a32e41a3152aecbf0b9c
nueng <166599504+imperresa@users.noreply.github.com>
2026-01-12 22:03:33 +0700

    fix database

--- client/src/component/HomePage.jsx
+++ client/src/component/HomePage.jsx
@@ -7,7 +7,7 @@ export function HomePage() {
 
   let homepage = async () => {
     let response = await axios.get(
-      `https://travel-tourist-rd45.vercel.app/trips?keywords=${search}`
+      `https://travel-tourist-9n19.vercel.app/trips?keywords=${search}`
     );
     setTrip(response.data.data);
     console.log(response.data);
